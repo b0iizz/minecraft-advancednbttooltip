539c95267ea9e0b8754c404816d317093f1cc765
B0IIZZ <70597779+b0iizz@users.noreply.github.com>
2022-04-19 22:29:18 +0200

    Convert Tooltip injection through ScreenMixin to CustomTooltipRenderer

--- src/main/java/me/b0iizz/advancednbttooltip/mixin/ScreenMixin.java
+++ src/main/java/me/b0iizz/advancednbttooltip/mixin/ScreenMixin.java
@@ -1,40 +1,34 @@
 package me.b0iizz.advancednbttooltip.mixin;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-import me.b0iizz.advancednbttooltip.AdvancedNBTTooltips;
+import me.b0iizz.advancednbttooltip.gui.CustomTooltipRenderer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
-import net.minecraft.client.item.TooltipContext;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
 
 @Mixin(Screen.class)
-public abstract class ScreenMixin {
+public abstract class ScreenMixin implements CustomTooltipRenderer {
 	
 	@Shadow
 	protected MinecraftClient client;
 
 	@Overwrite
 	public void renderTooltip(MatrixStack matrices, ItemStack stack, int x, int y) {
-		TooltipContext ctx = this.client.options.advancedItemTooltips ? TooltipContext.Default.ADVANCED
-				: TooltipContext.Default.NORMAL;
-		List<TooltipComponent> tooltip = stack.getTooltip(this.client.player, ctx).stream().map(Text::asOrderedText)
-				.map(TooltipComponent::of).collect(Collectors.toCollection(ArrayList::new));
-		stack.getTooltipData().map(TooltipComponent::of).ifPresent(tooltip::add);
-		
-		AdvancedNBTTooltips.getTooltip(stack, ctx, tooltip);	
-		this.renderTooltipFromComponents(matrices, tooltip, x, y);
+		this.renderTooltip(matrices, stack, x, y, this.client.options.advancedItemTooltips, null, this.client.player);
 	}
-
+	
+	@Override
+	public void renderComponents(MatrixStack matrices, ItemStack stack, List<TooltipComponent> components, int x, int y) {
+		this.renderTooltipFromComponents(matrices, components, x, y);
+	}
+	
 	@Shadow
 	public abstract void renderTooltipFromComponents(MatrixStack matrices, List<TooltipComponent> components, int x,
 			int y);
